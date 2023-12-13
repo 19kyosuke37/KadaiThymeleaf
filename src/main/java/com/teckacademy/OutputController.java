ef18822e0164c0c22199fcc673f50cc3fe989cd6
19kyosuke37 <19kyosuke37@gmail.com>
2023-12-13 23:26:50 +0900

    課題の提出

---
+++ src/main/java/com/teckacademy/OutputController.java
@@ -0,0 +1,19 @@
+package com.teckacademy;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class OutputController {
+
+    @PostMapping("/output")
+    public String getOutput(@RequestParam("val") String val,Model model) {
+        model.addAttribute("val",val);
+        return "output";
+    }
+
+
+
+}
